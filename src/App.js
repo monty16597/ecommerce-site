c441f7bb9813fb35f4e10beb25708fc88fc1a33d
Manjeetsinh Alonja <alonjamohit77@gmail.com>
2025-08-18 20:59:48 -0400

    Added Public_URL

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// src/App.js
+// src/app.js
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/Home';
@@ -6,17 +6,17 @@ import AboutPage from './pages/About';
 import NavBar from './components/Navbar';
 import BedSheetPage from './pages/BedSheet';
 import ProductDetailPage from './pages/ProductDetail';
-import ClothsPage from './pages/Cloths'; // Import the new component
+import ClothsPage from './pages/Cloths';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/bedsheets" element={<BedSheetPage />} />
-        <Route path="/cloths" element={<ClothsPage />} /> {/* Add the new route */}
+        <Route path="/cloths" element={<ClothsPage />} />
         <Route path="/product/:id" element={<ProductDetailPage />} />
       </Routes>
     </BrowserRouter>
